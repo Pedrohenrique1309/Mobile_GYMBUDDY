ff18d35cdce198db41f20b644c896606079233c6
GABRIEL SOUZA COSTA <24122537@dsn127m1242373.cfp127aluno.local>
2025-09-16 14:16:13 -0300

    Cores tema claro no arquivo Colors.kt ok

--- app/src/main/java/senai/sp/jandira/mobile_gymbuddy/ui/theme/Color.kt
+++ app/src/main/java/senai/sp/jandira/mobile_gymbuddy/ui/theme/Color.kt
@@ -9,3 +9,18 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Cores do tema claro
+val primaryLight = Color(0xFF000000)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val secondaryLight = Color(0xFF98000F)
+val onSecondaryLight = Color(0xFFFFFFFF)
+val tertiaryLight = Color(0xFF790009)
+val onTertiaryLight = Color(0xFFFFFFFF)
+val errorLight = Color(0xFF2C0001)
+val onErrorLight = Color(0xFFFFFFFF)
+val primaryContainerLight = Color(0xFF1B1B1B)
+val onPrimaryContainerLight = Color(0xFF848484)
+val backgroundLight = Color(0xFFF9F9F9)
+val onBackgroundLight = Color(0xFF1B1B1B)
+
