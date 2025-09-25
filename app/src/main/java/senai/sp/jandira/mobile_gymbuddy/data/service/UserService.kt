54b7517870a143f2de88de4710943f444bce2a7d
GABRIEL SOUZA COSTA <24122537@CFP127ALUNO.LOCAL>
2025-09-25 16:20:23 -0300

    Arquivo ImcScreen criado

--- app/src/main/java/senai/sp/jandira/mobile_gymbuddy/data/service/UserService.kt
+++ app/src/main/java/senai/sp/jandira/mobile_gymbuddy/data/service/UserService.kt
@@ -1,5 +1,3 @@
-// ARQUIVO: data/service/UsuarioService.kt
-
 package senai.sp.jandira.mobile_gymbuddy.data.service
 
 import senai.sp.jandira.mobile_gymbuddy.data.model.Usuario
@@ -9,16 +7,12 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
 import senai.sp.jandira.mobile_gymbuddy.data.model.LoginResponse
-import retrofit2.http.Headers // <-- MUDANÇA 1: Importe o Headers
+import retrofit2.http.Headers
 
 interface UsuarioService {
-
-    // MUDANÇA 2: Adicione a anotação @Headers logo acima do @POST
     @Headers("Content-Type: application/json")
     @POST("v1/gymbuddy/usuario")
     suspend fun cadastrarUsuario(@Body usuario: Usuario): Response<Void>
-
-    // A função de LOGIN continua a mesma, pois não envia 'body'
     @GET("v1/gymbuddy/usuario/login/email/senha")
     suspend fun logarUsuario(
         @Query("email") email: String,
